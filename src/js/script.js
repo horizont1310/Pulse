ea99c83eb49e9e616526e736a2e1ade8a73c815a
Ihor <hor@gmail.com>
2022-05-13 20:19:39 +0300

    Tiny slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
@@ -14,4 +14,25 @@ $(document).ready(function(){
             }
         ]
       });
+}); */
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+ /*  controlsText: [
+    '<img src="icons/left.svg">',
+    '<img src="icons/right.svg">'
+  ] */  
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
